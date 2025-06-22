22aa05133a7a3224e8771fd593c6411690fb7a2e
lutfor2000 <lutfar199296@gmail.com>
2025-06-22 17:04:13 +0600

    admin

--- AdminApp/database/migrations/0001_01_01_000000_create_users_table.php
+++ AdminApp/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->text('otp')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'user', 'editor'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
